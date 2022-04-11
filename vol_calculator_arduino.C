22bd7f6c71c179b7c0e84edf0fc7ecd50e51dffb
Luís Felipe Do Carmo Costa Tavares <48478593+LuisFelipeTs@users.noreply.github.com>
2022-04-11 14:59:07 -0300

    Update vol_calculator_arduino.C

--- vol_calculator_arduino.C
+++ vol_calculator_arduino.C
@@ -15,6 +15,7 @@ float sensorA_dist;
 float sensorB_dist;
 float sensorC_dist;
 
+float actual_vol;
 int response;
 //-- Sumário da variável response --
 // 0 - Nenhuma constatação
@@ -26,11 +27,12 @@ int response;
 //----------------------------------
 
 //Parâmetros Configuráveis
-const uint16_t cylinder_height = 1000;
-const uint16_t cylinder_radius = 50;
-const uint16_t critical_height = 120;
-const uint16_t extravasion_height = 850;
+const uint16_t cylinder_height = 50;
+const uint16_t cylinder_radius = 2;
+const uint16_t critical_height = 10;
+const uint16_t extravasion_height = 40;
 const int err_margin = cylinder_height * 0.10;
+const float pi = 3.1415;
 
 //Ler a distância 
 float readUDist(int sensor_PIN) {
@@ -77,39 +79,47 @@ int sensorProbl(int sA, int sB, int sC) {
 }
 
 //Gerar display do lcd
-void generateDisplay(String top_alert, float avg ) {
+void generateDisplay(String top_alert, float actual_vol ) {
   char linha1[17];
   char floatString[10];
   display_lcd.setCursor(0,0);
   display_lcd.print(top_alert);
   display_lcd.setCursor(0,1);
-  dtostrf(avg, 4, 2, floatString);
+  dtostrf(actual_vol, 4, 2, floatString);
   sprintf(linha1, "Volume: %s", floatString);
   display_lcd.print(linha1);
-  Serial.println(linha1);
+  Serial.println(actual_vol);
 }
   
 //Interpretar a resposta do código
-void interpResponse(int response) {
+void interpResponse(int response, float act_vol) {
   Serial.println(response);
   if(response == 0){
-	generateDisplay("Status: OK!", 0.12);
+	generateDisplay("--Status: OK!!--", act_vol);
   } else if(response == 1){
-    generateDisplay("**Nível Crítico**", 0.12);
+    generateDisplay(" Nivel Critico! ", act_vol);
     buzzControl(870,250,250);
   } else if(response == 2){
-    generateDisplay("**Nível Extravasão**", 0.12);
+    generateDisplay("Nivel Extravasao", act_vol);
     buzzControl(550,450,250);
   } else if(response == 3){
-    generateDisplay("**Sensor 1 Falhou!**", 0.12);
+    generateDisplay("Sensor 1 Falhou!", act_vol);
     buzzControl(1110,1000,1);
   } else if(response == 4){
-    generateDisplay("**Sensor 2 Falhou!**", 0.12);
+    generateDisplay("Sensor 2 Falhou!", act_vol);
     buzzControl(1110,1000,1);
   } else if(response == 5){
-    generateDisplay("**Sensor 3 Falhou!**", 0.12);
+    generateDisplay("Sensor 3 Falhou!", act_vol);
     buzzControl(1110,1000,1);
   }
+}
+
+
+//retorna o volume das esferas
+float getVolavg(int sensorA, int sensorB, int sensorC){
+  float sens_ave = (sensorA + sensorB + sensorC)/3; 
+  float base_area = pow(cylinder_radius, 2) * pi;
+  return base_area * sens_ave;
 }
 
 void setup()
@@ -124,14 +134,12 @@ void setup()
 
 void loop()
 {
-  display_lcd.setCursor(0,1);
-  
   sensorA_dist = readUDist(sensorA_PIN);
   sensorB_dist = readUDist(sensorB_PIN);
   sensorC_dist = readUDist(sensorC_PIN);
   response = getResponse(sensorA_dist, sensorB_dist, sensorC_dist);
-  interpResponse(response);
-  delay(500);
-  //display_lcd.clear();
+  actual_vol = getVolavg(sensorA_dist, sensorB_dist, sensorC_dist);
+  interpResponse(response, actual_vol);
+  delay(400);
   
 }
