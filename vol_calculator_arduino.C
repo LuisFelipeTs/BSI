5eead9e26048196dbe1ea094e2e121bf04607259
Luís Felipe Do Carmo Costa Tavares <48478593+LuisFelipeTs@users.noreply.github.com>
2022-04-11 13:34:51 -0300

    Update vol_calculator_arduino.C

--- vol_calculator_arduino.C
+++ vol_calculator_arduino.C
@@ -1,3 +1,10 @@
+// LUÍS FELIPE DO CARMO COSTA TAVARES
+// UFRPE
+// Internet das Coisas - IOT
+
+#include <stdio.h> 
+#include <LiquidCrystal.h>
+LiquidCrystal display_lcd = LiquidCrystal(7, 6, 5, 4, 3, 2);
 
 const int sensorA_PIN = 10;
 const int sensorB_PIN = 11;
@@ -19,11 +26,11 @@ int response;
 //----------------------------------
 
 //Parâmetros Configuráveis
-int cylinder_height = 1000;
-int cylinder_radius = 50;
-int critical_height = 120;
-int extravasion_height = 850;
-int err_margin = cylinder_height * 0.10;
+const uint16_t cylinder_height = 1000;
+const uint16_t cylinder_radius = 50;
+const uint16_t critical_height = 120;
+const uint16_t extravasion_height = 850;
+const int err_margin = cylinder_height * 0.10;
 
 //Ler a distância 
 float readUDist(int sensor_PIN) {
@@ -60,34 +67,54 @@ int getResponse(int sensorA, int sensorB, int sensorC) {
 }
 
 //Checar se os sensores estão ativos
-int sensorProbl(int sA, int sB, int sC){
-  int compareAB = abs(sB - sA);
-  int compareBC = abs(sB - sC);
-  
-  if (compareAB >= err_margin && compareBC < err_margin) { return 1;} 
-  else if (compareAB >= err_margin && compareBC >= err_margin) {return 2;} 
-  else if (compareBC >= err_margin && compareAB < err_margin){return 3;} 
+int sensorProbl(int sA, int sB, int sC) {
+  int compare_ab = abs(sB - sA);
+  int compare_bc = abs(sB - sC);
+  if (compare_ab >= err_margin && compare_bc < err_margin) { return 1;} 
+  else if (compare_ab >= err_margin && compare_bc >= err_margin) {return 2;} 
+  else if (compare_bc >= err_margin && compare_ab < err_margin){return 3;} 
   else {return 0;}
+}
+
+//Gerar display do lcd
+void generateDisplay(String top_alert, float avg ) {
+  char linha1[17];
+  char floatString[10];
+  display_lcd.setCursor(0,0);
+  display_lcd.print(top_alert);
+  display_lcd.setCursor(0,1);
+  dtostrf(avg, 4, 2, floatString);
+  sprintf(linha1, "Volume: %s", floatString);
+  display_lcd.print(linha1);
+  Serial.println(linha1);
 }
   
 //Interpretar a resposta do código
-void interpResponse() {
+void interpResponse(int response) {
+  Serial.println(response);
   if(response == 0){
+	generateDisplay("Status: OK!", 0.12);
   } else if(response == 1){
+    generateDisplay("**Nível Crítico**", 0.12);
     buzzControl(870,250,250);
   } else if(response == 2){
+    generateDisplay("**Nível Extravasão**", 0.12);
     buzzControl(550,450,250);
   } else if(response == 3){
+    generateDisplay("**Sensor 1 Falhou!**", 0.12);
     buzzControl(1110,1000,1);
   } else if(response == 4){
+    generateDisplay("**Sensor 2 Falhou!**", 0.12);
     buzzControl(1110,1000,1);
   } else if(response == 5){
+    generateDisplay("**Sensor 3 Falhou!**", 0.12);
     buzzControl(1110,1000,1);
   }
 }
 
 void setup()
 {
+  display_lcd.begin(16, 2);
   Serial.begin(9600);
   pinMode(buzz_PIN,OUTPUT);
   pinMode(sensorA_PIN,INPUT);
@@ -97,12 +124,14 @@ void setup()
 
 void loop()
 {
+  display_lcd.setCursor(0,1);
   
   sensorA_dist = readUDist(sensorA_PIN);
   sensorB_dist = readUDist(sensorB_PIN);
   sensorC_dist = readUDist(sensorC_PIN);
-  //response = getResponse(sensorA_dist, sensorB_dist, sensorC_dist);
-  interpResponse();
-  delay(800);
-  Serial.println( );
+  response = getResponse(sensorA_dist, sensorB_dist, sensorC_dist);
+  interpResponse(response);
+  delay(500);
+  //display_lcd.clear();
+  
 }
